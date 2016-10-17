03e60d85df20c28ff095f1f30e78ee4652942777
thomas1903 <tomandrewmills@gmail.com>
2016-10-17 13:53:40 +0100

    .

---
+++ week4/createSuperhero.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+        <form action="superheroDisplay.php" method="post">
+            <label>Name:</label><input type="text" name="name">
+            <label>Gender:</label><input type="text" name="gender">
+            <label>Superhero power:</label><input type="text" name="power">
+            <input type="submit" value="Submit">
+        </form>
+    </body>
+</html>
